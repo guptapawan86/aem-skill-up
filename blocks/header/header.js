aaf6b22037d4bf3fccb9a397a06ff1233d305d90
guptapawan <guptapawan@adobe.com>
2025-08-11 02:33:26 +1000

    commerce delete - 3-2

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -382,5 +382,5 @@ export default async function decorate(block) {
     navSections,
     () => !isDesktop.matches && toggleMenu(nav, navSections, false),
   );
-  renderAuthDropdown(navTools);
+  //renderAuthDropdown(navTools);
 }

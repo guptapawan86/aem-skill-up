8980cb7c70bd56e8a1381d9d1e8e34de2b557ba0
guptapawan <guptapawan@adobe.com>
2025-09-10 18:33:31 +0800

    updated google maps

--- blocks/store-locator/store-locator.js
+++ blocks/store-locator/store-locator.js
@@ -4,7 +4,7 @@ export async function initMap() {
   const { Map } = await google.maps.importLibrary('maps');
 
   const map = new Map(document.getElementById('locator-map'), {
-    center: { lat: 36.121, lng: -115.170 },
+    center: { lat: 1.297373, lng: 103.857975 },
     zoom: 17,
     disableDefaultUI: true,
     keyboardShortcuts: false,
